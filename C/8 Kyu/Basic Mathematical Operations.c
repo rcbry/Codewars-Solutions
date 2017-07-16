d4aa28e16cd69f79947a4729e6bfab0c590e8cbc
rcbry <yrcbryyrcbry@gmail.com>
2017-07-16 18:50:21 -0300

    Create Basic Mathematical Operations.c

---
+++ C/8 Kyu/Basic Mathematical Operations.c
@@ -0,0 +1,12 @@
+int basic_op(char op, int value1, int value2) 
+{
+  switch (op)
+  {
+    case '+': return value1 + value2;
+    case '-': return value1 - value2;
+    case '*': return value1 * value2;
+    case '/': if (value2 != 0) return value1 / value2;
+    default: return 0;
+  }
+  return 0;
+}
