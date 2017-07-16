d41f4296ce29cedb46fe00bb101e0584939c8369
rcbry <yrcbryyrcbry@gmail.com>
2017-07-16 19:00:49 -0300

    Create Don't give me five!.c

---
+++ C/7 Kyu/Don't give me five!.c
@@ -0,0 +1,12 @@
+int dontGiveMeFive(int start, int end)
+{
+  int x=0; // counter
+  for (int i=start;i<=end;i++)
+  {
+    if (i%5==0 && i%10!=0);
+    else if (i<=59 && i>=50);
+    else
+      x++;
+  }
+  return x;
+}
