624c2bce81fed7d900204084780f7106f49271c8
rcbry <yrcbryyrcbry@gmail.com>
2017-07-16 18:54:06 -0300

    Create Parse nice int from char problem.c

---
+++ C/8 Kyu/Parse nice int from char problem.c
@@ -0,0 +1,7 @@
+int getAge(const char *inputString)
+{
+  int i; return i = inputString[0] - '0';
+  // Paragraph 2.2.1: "In both the source and execution basic character sets,
+  // the value of each character after 0 in the above list of decimal digits
+  // shall be one greater than the value of the previous."
+}
