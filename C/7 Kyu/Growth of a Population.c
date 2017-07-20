e85efee3c958f41b1cf44cdd717f7aa25e12e2a2
rcbry <yrcbryyrcbry@gmail.com>
2017-07-20 16:04:25 -0300

    Update Growth of a Population.c

--- C/7 Kyu/Growth of a Population.c
+++ C/7 Kyu/Growth of a Population.c
@@ -3,7 +3,7 @@ int nbYear(int p0, double percent, int aug, int p)
   int years = 0;
   while (1)
   {
-    p0 = p0 + p0 * percent / 100 + aug;
+    p0 += p0 * percent / 100 + aug;
     years++;
     if (p0 >= p)
       break;
