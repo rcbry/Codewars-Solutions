4047303dfc9b3f24894c2e220cc0c33b48135341
rcbry <yrcbryyrcbry@gmail.com>
2017-07-22 19:59:52 -0300

    Update Find twins.c

--- C/7 Kyu/Find twins.c
+++ C/7 Kyu/Find twins.c
@@ -2,9 +2,11 @@
 
 int elimination(const int *arr, size_t arr_size, int* result)
 {
-  for(size_t i = 0; i < arr_size; ++i)
-      for(size_t j = i + 1; j < arr_size; ++j)
-          if (arr[i] == arr[j])
-            { *result = arr[i]; return 1; }
+  if (arr == NULL || result == NULL || arr_size == 0) return 0;
+  else
+    for (size_t i = 0; i < arr_size; ++i)
+      for (size_t j = i + 1; j < arr_size; ++j)
+        if (arr[i] == arr[j])
+          { *result = arr[i]; return 1; }
   return 0;
 }
