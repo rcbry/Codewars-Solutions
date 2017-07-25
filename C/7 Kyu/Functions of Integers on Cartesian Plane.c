b936e064e602bf5ffc6c1119c33569d44b7eb04d
rcbry <yrcbryyrcbry@gmail.com>
2017-07-25 19:19:16 -0300

    Create Functions of Integers on Cartesian Plane.c

---
+++ C/7 Kyu/Functions of Integers on Cartesian Plane.c
@@ -0,0 +1,3 @@
+unsigned long long sumin(unsigned long long n)  { return ((n*(n+1))*(((2*n)+1))/6); }
+unsigned long long sumax(unsigned long long n)  { return ((n*(n+1))*(((4*n)-1))/6); }
+unsigned long long sumsum(unsigned long long n) { return (n*(n*(n+1))); }
